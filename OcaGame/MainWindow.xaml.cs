38a5f0804c2f31ae2cfd2f5320a41700f3ee8de5
Mario Alberto Jimenez Jimenez <90919789+MarioAlJim@users.noreply.github.com>
2022-09-29 15:53:05 -0500

    Proyecto primera versión

---
+++ OcaGame/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OcaGame
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_inicioSesion_Click(object sender, RoutedEventArgs e)
+        {
+            OcaBussinessLogic.Authentication autenti = new OcaBussinessLogic.Authentication();
+            String username = txt_usuario.Text;
+            String password = txt_contrasenia.Password;
+            bool exist;
+            exist = autenti.login(username, password);
+            if (exist == true)
+            {
+                MainMenu mainMenu = new MainMenu();
+                mainMenu.Show();
+            }
+            else
+            {
+                MessageBox.Show("No se encontro el usuario");
+            }
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.Show();
+        }
+    }
+}
