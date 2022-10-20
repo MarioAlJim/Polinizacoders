5432947e91b11bb3d56ae0e1f2df1e654e059738
Mario Alberto Jimenez Jimenez <90919789+MarioAlJim@users.noreply.github.com>
2022-10-19 21:15:56 -0500

    Actualización estructura de servidor

---
+++ OcaWCF/Authentication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace OcaWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class Authentication : IAuthentication
+    {
+        public bool login(string userName, string password)
+        {
+            OcaBussinessLogic.Authentication authentication = new OcaBussinessLogic.Authentication();
+            Boolean sucess = false;
+            sucess= authentication.login(userName, password);
+            return sucess
+;        }
+    }
+}
