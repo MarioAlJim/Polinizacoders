4861f644b60304b1a17bdd48c1be95c39265d49a
Mario Alberto Jimenez Jimenez <90919789+MarioAlJim@users.noreply.github.com>
2022-11-03 14:31:56 -0600

    Actualizacion

---
+++ OcaBussinessLogic/UsersAdministration.cs
@@ -0,0 +1,34 @@
+﻿using OcaDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcaBussinessLogic
+{
+    public class UsersAdministration
+    {
+
+        public Boolean SignUpUser(string nickname, string password)
+        {
+            Console.WriteLine("*** Add User Starts ***");
+
+            using (var context = new OcaDBEntities())
+            {
+                //Log DB commands to console
+                context.Database.Log = Console.WriteLine;
+
+                //Add a new student and address
+                var newStudent = context.Users.Add(new User() { Nickname = nickname, Password = password });
+                context.SaveChanges(); // Executes Insert command
+                Console.WriteLine("*** Add User Succes");
+
+            }
+
+            return true;
+        }
+
+
+    }
+}
